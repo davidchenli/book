994f2c6fccf7d95bbed608495c8665ac17ef5960
david_chenlee <david_chenlee@mail.chungyo.net>
2024-07-13 11:27:08 +0800

    test

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
-# import streamlit as st
-#
-# # Set the configuration for the Streamlit app
-# st.set_page_config(
-#     page_title="雲水書車-自動化小工具",  # Page title
-#     page_icon="🧊",  # Page icon
-#     layout="wide",  # Use a wide layout
-#     initial_sidebar_state="expanded",  # Sidebar expanded by default
-#     menu_items={  # Customize the menu items
-#         'Get Help': 'https://www.extremelycoolapp.com/help',  # Link for getting help
-#         'Report a bug': "https://stackoverflow.com/questions/42830557/git-remote-add-origin-vs-remote-set-url-origin",  # Link for reporting a bug
-#         'About': "# This is a header. This is an *extremely* cool app!"  # About section
-#     }
-# )
-#
-# # Add content to the app
-# st.title("雲水書車-自動化小工具")
-# st.write("歡迎來到這個非常酷的應用程序！")
-#
-# # Add some interactive elements
-# if st.button('Click Me'):
-#     st.write('Button clicked!')
-#
-# st.sidebar.header('Sidebar Title')
-# st.sidebar.write('Sidebar content goes here.')
+import streamlit as st
+
+st.set_page_config(
+    page_title="雲水書車-自動化小工具",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+
+st.title("雲水書車-自動化小工具")
+
+st.sidebar.header('歡迎來到 雲水書車 小工具')
+st.sidebar.write('點擊上列選項開始！')
